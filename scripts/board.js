cea482988e68de6841be8f55c833950936c93205
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2024-10-05 13:15:28 +0200

    board_update

--- scripts/board.js
+++ scripts/board.js
@@ -1,4 +1,47 @@
 
+let BASE_URL = 'https://join-376-dd26c-default-rtdb.europe-west1.firebasedatabase.app/';
+
+async function loadTasks() {
+    let tasks = await fetch(BASE_URL + "/tasks.json")
+    let tasksJson = await tasks.json();
+    // document.getElementById('in-progress', 'await-feedback', 'done').innerHTML = "";
+    
+    for (let i = 0; i < tasksJson.length; i++) {
+        console.log(tasksJson[i]);
+        let category = tasksJson[i].category;
+        let classCategory = checkCategory(category);
+        let title = tasksJson[i].title;
+        let description = tasksJson[i].description;
+        let list = tasksJson[i].list;
+        let assignedTo = tasksJson[i].assignedTo;
+        let firstLetterNames = findFirstNameLetter(assignedTo);
+        console.log(assignedTo);
+        document.getElementById(`${list}`).innerHTML += getTask(category, classCategory, title, description, firstLetterNames);        
+    }
+}
+
+function checkCategory(category) {
+    if(category == 'Technical Task') {
+        classCategory = 'technical-task';
+    } else {
+        classCategory = 'user-story';
+    }
+    return classCategory;
+}
+
+function findFirstNameLetter(assignedTo) {
+    for (let i = 0; i < assignedTo.length; i++) {
+        let firstName = assignedTo[i].firstName;
+        firstName = firstName[0];
+
+        let lastName = assignedTo[i].lastName;
+        lastName = lastName[0];
+
+        let firstLetterNames = firstName + lastName;
+        return firstLetterNames;
+    }
+}
+
 function toggleSubtask() {
     let refIcon = document.getElementById('click-subtaks');
     let refSubtask = document.getElementById('subtasks');  
--- scripts/template.js
+++ scripts/template.js
@@ -1,3 +1,30 @@
+
+function getTask(category, classCategory, title, description, firstLetterNames) {
+    return `
+    <div class="task-card">
+        <div>
+            <label class="category-${classCategory}">${category}</label>
+        </div>
+        <h4>${title}</h4>
+        <p>${description}</p>
+        <div class="subtasks" id="subtasks">
+
+        </div>
+        <div class="task-footer">
+            <div class="assigned-task">
+                <div class="assigned-user" style="background-color: #462F8A;">
+                <p>${firstLetterNames}</p>
+            </div>
+            <div class="assigned-user">
+                <p>EM</p>
+            </div>
+        </div>
+        <button onclick="toggleSubtask()"><img id="click-subtaks" src="./assets/icons/priority_open_down_icon.png" alt="open"></button>
+        </div>
+    </div>
+    `
+}
+
 function getSubtask() {
     return `
     <div class="progress-border"><div id="subtask-progress" class="subtask-progress" style="width: 50%;"></div></div>
