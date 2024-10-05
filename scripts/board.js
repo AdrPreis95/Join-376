15ab943f720bb4422adb1a030865f2cfba070c03
HPetersen2 <149525216+HPetersen2@users.noreply.github.com>
2024-10-05 10:54:14 +0200

    board_update

--- scripts/board.js
+++ scripts/board.js
@@ -1,7 +1,13 @@
-function changeColorAssignedUser() {
-    let colors = ['#FC71FF', '#462F8A', '#FF7A00', '#1FD7C1'];
-    let colorsLength = colors.length;
-    let indexColor = (Math.random() * colorsLength);
 
-    document.getElementById('assigned1').style.background = colors[indexColor];
+function toggleSubtask() {
+    let refIcon = document.getElementById('click-subtaks');
+    let refSubtask = document.getElementById('subtasks');  
+
+    if(refIcon.getAttribute('src') == './assets/icons/priority_open_down_icon.png') {
+        refIcon.setAttribute('src', './assets/icons/urgent_icon.png');
+        refSubtask.innerHTML = getSubtask();
+    } else if(refIcon.getAttribute('src') == './assets/icons/urgent_icon.png') {
+        refIcon.setAttribute('src', './assets/icons/priority_open_down_icon.png');
+        refSubtask.innerHTML = "";
+    }
 }
---
+++ scripts/template.js
@@ -0,0 +1,6 @@
+function getSubtask() {
+    return `
+    <div class="progress-border"><div id="subtask-progress" class="subtask-progress" style="width: 50%;"></div></div>
+    <p>1/2 Subtasks</p>
+    `
+}
