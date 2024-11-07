2de50a36a7dcae91a5092302c2e9e2a29a489815
Jonas M dos Santos <jon.mag.santos@gmail.com>
2024-11-07 04:36:55 +0100

    first version add_task_overlay

--- scripts/add_task_overlay.js
+++ scripts/add_task_overlay.js
@@ -2,6 +2,13 @@
 function openTaskOverlay() {
   document.getElementById("taskoverlay").classList.remove("hidden");
   document.body.classList.add("overlay-active");
+  document.querySelectorAll("#overlayContent").forEach(function(iframe) {
+    var iframeDoc = iframe.contentDocument || iframe.contentWindow.document;
+    var noneElements = iframeDoc.querySelectorAll(".sidebar,  .header, .responsive-footer");
+    noneElements.forEach(function(noneElement) {
+        noneElement.style.display = "none";
+    });
+});
 }
 
 function closeTaskOverlay() {
