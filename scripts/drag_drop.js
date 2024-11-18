dd3af71ae2d384634ae85e8c6cb25c8ff82d4cda
Jonas M dos Santos <jon.mag.santos@gmail.com>
2024-11-19 00:01:49 +0100

    drag and drop updated

--- scripts/drag_drop.js
+++ scripts/drag_drop.js
@@ -38,7 +38,7 @@ function pickup(event) {
     moving.style.height = moving.clientHeight + "px";
     moving.style.width = moving.clientWidth + "px";
     moving.style.position = 'fixed';
-    moving.style.zIndex = '-10'; 
+    moving.style.zIndex = '-10';
 }
 
 function move(event) {
@@ -75,24 +75,26 @@ function drop(event) {
 
         // reset our element
         removeDragging(moving.id);
-        moving = resetElement(moving);        
+        moving = resetElement(moving);
     }
 }
 
-function setTargetList(target){
-    if (target.at('div.list').childNodes[3]){
-        return target.at('div.list').childNodes[3];
-    } else {
-        for (let index = 0; index < target.length; index++) {
-            const element = target[index];
-            if (listNames.includes(element.id))
-                return element;
+function setTargetList(target) {
+    if (target.at('div.list').childNodes[3] != undefined) {
+        if (listNames.includes(target.at('div.list').childNodes[3].id)) {
+            return target.at('div.list').childNodes[3];
         }
-
     }
+
+    for (let index = 0; index < target.length; index++) {
+        const element = target[index];
+        if (listNames.includes(element.id))
+            return element;
+    }
+
 }
 
-function checkTargetList(targetList, moving){
+function checkTargetList(targetList, moving) {
     if (targetList) {
         if (!targetList.contains(moving)) {
             let list = targetList.className;
@@ -116,8 +118,8 @@ function resetElement(moving) {
     return null;
 }
 
-document.addEventListener('mousemove', function(e) {
-    if (moving){
+document.addEventListener('mousemove', function (e) {
+    if (moving) {
         moving.style.left = e.pageX + 'px';
         moving.style.top = e.pageY + 'px';
     }
